f469eefd6fc6f52b4fe0868df4c97191e6d64989
面川泰明 <omokawa@senk-inc.co.jp>
2019-11-23 23:07:35 +0900

    ログインID生成モジュールメモリ使用量改善

--- lib/CreateDummy/LoginId.pm
+++ lib/CreateDummy/LoginId.pm
@@ -4,45 +4,22 @@ use warnings;
 use utf8;
 
 use String::Random;
+use Devel::Size qw(size total_size);
 
 sub new {
-  my ($class, %params) = @_;
+  my ($class) = @_;
 
   bless {
-    number => $params{number},
     ids => {},
+    sr => String::Random->new,
   }, $class;
-}
-
-sub create {
-  my ($self) = @_;
-
-  my $sr = String::Random->new;
-
-  for my $number (1..$self->{number}) {
-
-    #  英小文字6桁の重複なしランダム文字列を生成
-    my $str = "";
-    while(1) {
-      $str = $sr->randregex('[a-z]{6}');
-
-      # 生成した文字列が一意である場合は文字列を格納
-      unless (exists $self->{ids}{$str}) {
-        $self->{ids}{$str} = "";
-        last;
-      }
-    }
-  }
 }
 
 sub get {
   my ($self) = @_;
 
-  my ($k, $v) = each %{$self->{ids}};
-
-  delete $self->{ids}{$k};
-
-  return $k;
+  #  英小文字6桁の重複なしランダム文字列を生成
+  return $self->{sr}->randregex('[a-z]{6}');
 }
 
 1;
