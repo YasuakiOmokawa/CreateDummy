afe03958f2888b611360b192fba881dbe69fe4c3
面川泰明 <omokawa@senk-inc.co.jp>
2019-12-01 17:34:50 +0900

    マッチの行数をa to z の辞書並び順に変更

--- lib/Match/InnerTable.pm
+++ lib/Match/InnerTable.pm
@@ -12,15 +12,8 @@ use Fcntl;
 sub new {
   my ($class) = @_;
 
-  my $dir = "$FindBin::Bin/../tmp/tmp$$";
-  my $file = "${dir}/inner_table.cdb";
-  mkpath $dir unless -d $dir;
-
-  my $cdb = new CDB_File ("$file.cdb", "$file.$$")
-    or croak "new CDB_File failed $file: $!\n";
-
   bless {
-    table => $cdb,
+    table => {},
   }, $class;
 }
 
@@ -28,15 +21,15 @@ sub insert {
   my ($self, %params) = @_;
 
   # データ格納
-  $self->{table}->insert($params{key}, $params{value});
+  $self->{table}{$params{key}} = $params{value};
 }
 
 sub get {
   my ($self, $key) = @_;
 
   my $v = undef;
-  if ($self->{table}->EXISTS($key)) {
-      $v = $self->{table}->FETCH($key);
+  if (exists $self->{table}{$key}) {
+      $v = $self->{table}{$key};
   }
 
   return $v;
@@ -45,4 +38,3 @@ sub get {
 1;
 __END__
 
-  $cdb->finish;
